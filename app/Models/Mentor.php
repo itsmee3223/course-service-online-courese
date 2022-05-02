a8a1fda50e5bfc21b53b99b1d109d6bc4f2c18c4
itsmee3223 <tamvam.pemberani@gmail.com>
2022-05-02 22:05:18 +0700

    model image course dan mentor

---
+++ app/Models/Mentor.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mentor extends Model
+{
+    protected $table = 'mentors';
+
+    protected $fillable = [
+        'name', 'profile', 'email', 'profession'
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:m:s',
+        'updated_at' => 'datetime:Y-m-d H:'
+    ];
+}
