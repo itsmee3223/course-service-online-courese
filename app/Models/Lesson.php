73668b4e0d9f82d78a25dd65b3a61d89b54f7a3b
itsmee3223 <tamvam.pemberani@gmail.com>
2022-05-03 23:33:52 +0700

    Fix model namespace

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Leeson extends Model
 {
+    use HasFactory;
     protected $table = 'lessons';
 
     protected $fillable = [
