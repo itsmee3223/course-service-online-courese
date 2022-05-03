7aa2a51bc74ae5493897e373c718679d34a44de0
itsmee3223 <tamvam.pemberani@gmail.com>
2022-05-04 00:17:29 +0700

    Course Controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MentorController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,9 @@ Route::get('mentors/{id}', [MentorController::class, 'show']);
 Route::post('mentors', [MentorController::class, 'create']);
 Route::put('mentors/{id}', [MentorController::class, 'update']);
 Route::delete('mentors/{id}', [MentorController::class, 'destroy']);
+
+Route::post('courses', [CourseController::class, 'create']);
+Route::put('courses/{id}', [CourseController::class, 'update']);
+Route::get('courses/{id}', [CourseController::class, 'show']);
+Route::get('courses', [CourseController::class, 'index']);
+Route::delete('courses/{id}', [CourseController::class, 'destroy']);
