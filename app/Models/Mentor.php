2d677b1f8399f002a77970d9c4a80cab1967d785
itsmee3223 <tamvam.pemberani@gmail.com>
2022-05-03 23:16:00 +0700

    Mentor Controller

--- app/Models/Mentor.php
+++ app/Models/Mentor.php
@@ -1,19 +1,21 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Mentor extends Model
 {
+    use HasFactory;
     protected $table = 'mentors';
-
-    protected $fillable = [
-        'name', 'profile', 'email', 'profession'
-    ];
 
     protected $casts = [
         'created_at' => 'datetime:Y-m-d H:m:s',
-        'updated_at' => 'datetime:Y-m-d H:'
+        'updated_at' => 'datetime:Y-m-d H:m:s'
+    ];
+
+    protected $fillable = [
+        'name', 'profile', 'email', 'profession'
     ];
 }
